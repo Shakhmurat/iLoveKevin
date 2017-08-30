7bce667b893c1d8c907d922f1c2ab20e043186d9
RocketMan <rocketman@Shaaa.local>
2017-08-30 22:11:34 +0600

    deleted jquery, bootstrap, extra code. Fixed markup, fixed styles

--- frontend/scripts/main/main.js
+++ frontend/scripts/main/main.js
@@ -1,8 +1,6 @@
 'use strict';
 
-import myModule from './module-to-import';
-
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
 
   var swiper = new Swiper('.swiper-container', {
     pagination: '.swiper-pagination',
@@ -10,21 +8,70 @@ $(document).ready(function() {
     autoHeight: true
   });
 
-  $('.form-control').on('change keyup', function () {
-  	if ($('.js-validate-name').val().length > 0 && +
-  		+ $('.js-validate-pass').val().length > 10 && + 
-  		+ $('.js-validate-check').is(':checked')) {
-  		$('.form-inline').find('[type="submit"]').removeClass('disabled');	
-  	} else {
-  		$('.form-inline').find('[type="submit"]').addClass('disabled');
-  	}
+  var inputControls = document.querySelectorAll('.form-control'),
+      nameField = document.querySelector('.js-validate-name'),
+      passField = document.querySelector('.js-validate-pass'),
+      checkField = document.querySelector('.js-validate-check'),
+      submit = document.querySelector('.form-inline [type="submit"]');
+
+  inputControls.forEach(function (el) {
+    el.addEventListener('keyup', formValidate)
+    el.addEventListener('change', formValidate)
+  })
+
+  function formValidate(){
+    if(nameField.value.length && passField.value.length > 10 && checkField.checked) {
+      submit.classList.remove('disabled')
+    } else {
+      submit.classList.add('disabled')
+    }
+  }
+
+  function scrollIt(destination, duration = 500) {
+
+    function easing(t) { 
+      return t * (2 - t);
+    }
+
+    const start = window.pageYOffset;
+    const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+
+    const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+    const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
+    const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
+
+    if ('requestAnimationFrame' in window === false) {
+      window.scroll(0, destinationOffsetToScroll);
+      return;
+    }
+
+    function scroll() {
+      const now = 'now' in window.performance ? performance.now() : new Date().getTime();
+      const time = Math.min(1, ((now - startTime) / duration));
+      const timeFunction = easing(time);
+      window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
+
+      if (window.pageYOffset === destinationOffsetToScroll) {
+        return;
+      }
+
+      requestAnimationFrame(scroll);
+    }
+
+    scroll();
+  }
+
+  var navHeaderItems = document.querySelectorAll('.header-nav .link');
+
+  navHeaderItems.forEach(function(el){
+    el.addEventListener('click', function(e){
+      e.preventDefault();
+      var target = e.currentTarget;
+      scrollIt(document.querySelector(target.hash));
+    })
   });
 
-  $('.header a').on('click', function () {
-    $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top
-    }, 500);
-  });
 
 });
 
